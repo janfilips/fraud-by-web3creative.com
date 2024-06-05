5a4cf4c3e7ba8cbe1b8dd709848391cdfd3efe50
Surjith S M <surjithctly@gmail.com>
2024-06-05 20:23:06 +0530

    dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
-        sans: [
-          "Bricolage Grotesque Variable",
-          "Inter Variable",
-          "Inter",
-          ...defaultTheme.fontFamily.sans,
-        ],
+        sans: ["Bricolage Grotesque Variable", "Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
         stock: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        gray: colors.neutral,
       },
     },
   },
